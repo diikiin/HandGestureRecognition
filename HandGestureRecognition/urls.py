c28457a7c7ed64fe1768d7abde65a30f8e700e9c
diikiin <88001773+diikiin@users.noreply.github.com>
2022-11-28 20:41:27 +0600

    added new app for authorization users (#1)
    * added new app for authorization users
    
    * added new app for authorization users
    
    Co-authored-by: Maqsat <MAmangeldi@one.kz>

--- HandGestureRecognition/urls.py
+++ HandGestureRecognition/urls.py
@@ -9,12 +9,21 @@ from hand_gesture.views import *
 router = routers.DefaultRouter()
 router.register(r'hand-gesture', HandGestureViewSet)  # http://127.0.0.1:8000/api/hand-gesture/
 router.register(r'translation', TranslationViewSet)  # http://127.0.0.1:8000/api/translation/
+from hand_gesture.views import HandGestureImageAPIView
+from authenticaiton.views import RegisterView,LoginView,UserView,LogoutView
+    # , HandGestureVideoAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/image/", ImageAPIView.as_view()),
     path("api/video/", VideoAPIView.as_view()),
     path("api/", include(router.urls)),
+    path("api/image", HandGestureImageAPIView.as_view()),
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
+    # path("api/video", HandGestureVideoAPIView.as_view())
 ]
 
 if settings.DEBUG:
